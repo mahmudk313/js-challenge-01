c19b54eab9873f4f8760ad7660ecce6f5a4b5840
MahmudK-PHNest <mahmudkenway@gmail.com>
2022-04-18 20:06:15 +0430

    assemble js pt1, receiving inputs and values
    -define selectors for elements
    -define an object for holding variables
    -define handlers for type, cost, date
    -can't get type from radio inputs

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//Query Selectors
+let inputBox = document.querySelector('.input_box');
+let radioInput = inputBox.querySelector('input[type=radio][name=radio]:checked');
+let inputCost = inputBox.querySelector('#ffi1');
+let submitBtn = inputBox.querySelector('.sub-btn');
+let day = inputBox.querySelector('#d-day');
+let month = inputBox.querySelector('#d-month');
+let year = inputBox.querySelector('#d-year');
+
+//JS
+let fin = {} //type,cost,date,detail
+
+submitBtn.addEventListener('click' , function(e) {
+    e.preventDefault();
+    typeHandler();
+    costHandler();
+    dateHandler();
+})
+
+
+let typeHandler = () => {
+    fin.type = radioInput;
+}
+let costHandler = () => {
+    fin.cost = parseInt(inputCost.value);
+    inputCost.value = '';
+}
+let dateHandler = () => {
+    fin.date = {
+        year : year.value,
+        month : month.value,
+        day : day.value
+    }
+    year.value = month.value = day.value = ''
+    console.log(fin)
+    
+}
